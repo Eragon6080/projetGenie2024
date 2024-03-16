81603e524dfd0eb8116838b51c7f62c20e8998ab
Gaillard Matthys <matthysgaillard@gmail.com>
2024-03-16 17:17:31 +0100

    Ajout de la page de cours résolution du nom qui ne s'affichait pas

--- pythonProject/genieLogiciel/polls/views.py
+++ pythonProject/genieLogiciel/polls/views.py
@@ -13,12 +13,16 @@ def index(request) -> HttpResponse:
     return render(request, 'submitSubject.html', context)
 
 
+# obliger de passer tous les élements nécessaires dans le context donc, attention aux id
+
 def cours(request) -> HttpResponse:
     context = {
         'cours1': "IDS",
-        'description1':"INFOB331",
+        'description1': "INFOB331",
         'cours2': "Mémoire",
         'description2': "INFOB332",
-        'title': 'Cours'
+        'title': 'Cours',
+        'prenom': "Matthys",
+        'role': "Etudiant"
     }
     return render(request, 'cours.html', context)
