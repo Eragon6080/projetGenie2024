08ead3cba47bc2c31cbf848f2b8259e310d7b356
Gaillard Matthys <matthysgaillard@gmail.com>
2024-03-16 17:09:19 +0100

    Ajout de la page pour les cours

--- pythonProject/genieLogiciel/polls/views.py
+++ pythonProject/genieLogiciel/polls/views.py
@@ -5,6 +5,20 @@ from django.template import loader
 
 # Create your views here.
 
-def index(request):
-    template = loader.get_template('submitSubject.html')
-    return HttpResponse(template.render())
+def index(request) -> HttpResponse:
+    context = {
+        'prenom': "Matthys",
+        'role': "Etudiant"
+    }
+    return render(request, 'submitSubject.html', context)
+
+
+def cours(request) -> HttpResponse:
+    context = {
+        'cours1': "IDS",
+        'description1':"INFOB331",
+        'cours2': "Mémoire",
+        'description2': "INFOB332",
+        'title': 'Cours'
+    }
+    return render(request, 'cours.html', context)
