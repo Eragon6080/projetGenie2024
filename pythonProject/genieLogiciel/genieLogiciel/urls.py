627c5d37deb2108821ab6ce636c01e4a436c55fb
Gaillard Matthys <matthysgaillard@gmail.com>
2024-03-16 13:59:50 +0100

    Le logo permet de retourner vers l'accueil du site

--- pythonProject/genieLogiciel/genieLogiciel/urls.py
+++ pythonProject/genieLogiciel/genieLogiciel/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('', include('polls.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
