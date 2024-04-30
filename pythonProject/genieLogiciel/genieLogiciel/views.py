552ff28c64c5ca4d39fb73cbf1a90f258cb96503
Gaillard Matthys <92369521+Eragon6080@users.noreply.github.com>
2024-04-30 13:15:53 +0200

    Réorganisation des fichiers

--- pythonProject/genieLogiciel/genieLogiciel/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpRequest
-from django.shortcuts import redirect, render
-
-
-
-def redirect_to_polls(request) -> HttpRequest:
-    return redirect("/polls")
-
-
--- pythonProject/genieLogiciel/polls/utils/date.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-
-
-def get_today_date():
-    return f"{date.today().day}/{date.today().month}/{date.today().year}"
