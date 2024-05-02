a0955fcff9b31368bf73e5ee249aa14920bff2d1
Gaillard Matthys <92369521+Eragon6080@users.noreply.github.com>
2024-05-02 13:14:44 +0200

    Git merge effectué

--- genieLogiciel/entrypoint.sh
+++ genieLogiciel/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Attendre que la base de données soit prête
-
-
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 &
 exec "$@"
