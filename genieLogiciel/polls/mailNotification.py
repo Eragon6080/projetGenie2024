af512aa603f6b0cc89b2d5aab213d1d47d2d2c70
Gaillard Matthys <92369521+Eragon6080@users.noreply.github.com>
2024-05-12 16:47:19 +0200

    Fin des commentaires dans la partie email

--- genieLogiciel/polls/mailNotification.py
+++ genieLogiciel/polls/mailNotification.py
@@ -4,11 +4,18 @@ from django.core.mail import BadHeaderError, send_mail
 from django.http import HttpResponse
 
 
-def sendMail(subject, message,to_mail=None):
+def sendMail(subject:str, message:str,to_mail=None)->HttpResponse:
+    """
+    Formulaire permettant d'envoyer une notification sous forme de mail
+    :param subject: Sujet du mail
+    :param message: message du mail
+    :param to_mail: adresse mail du destinataire
+    :return: Réponse pour savoir si le mail a été envoyé ou non
+    """
     if to_mail is None:
         to_mail = [os.getenv('EMAIL_DEFAULT_RECIPIENT')]
-    subject = subject
-    message = message
+    subject:str = subject
+    message:str = message
 
     if subject and message:
         try:
